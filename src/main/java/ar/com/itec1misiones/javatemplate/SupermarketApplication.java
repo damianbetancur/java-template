a5dfe5f88666d41bae5d5deca86ae797267afa3b
Mauro Britez <maurobritez96@gmail.com>
2025-09-12 19:23:51 -0300

    feat: add main app

---
+++ src/main/java/ar/com/itec1misiones/javatemplate/SupermarketApplication.java
@@ -0,0 +1,17 @@
+package ar.com.itec1misiones.javatemplate;
+
+import ar.com.itec1misiones.javatemplate.view.JavaFxApp;
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SupermarketApplication.class, args);
+        Application.launch(JavaFxApp.class, args);
+    }
+
+}
