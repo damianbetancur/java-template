7ed6e6385a706c0cb801c5b7b4cc4a0ad502df7c
Mauro Britez <maurobritez96@gmail.com>
2025-09-12 19:21:36 -0300

    feat: add models

---
+++ src/main/java/ar/com/itec1misiones/javatemplate/model/Empleado.java
@@ -0,0 +1,13 @@
+package ar.com.itec1misiones.javatemplate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Empleado {
+    private String nombre;
+    private String apellido;
+}
---
+++ src/main/java/ar/com/itec1misiones/javatemplate/model/LineaDeVenta.java
@@ -0,0 +1,23 @@
+package ar.com.itec1misiones.javatemplate.model;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class LineaDeVenta {
+    private int unidad;
+    private float precioUnitario;
+    private float prectioTotal;
+    private Producto producto;
+    private Venta venta;
+
+    public LineaDeVenta(Producto producto, int unidad, float precioUnitario) {
+        this.producto = producto;
+        this.unidad = unidad;
+        this.precioUnitario = precioUnitario;
+
+        this.prectioTotal = this.precioUnitario * this.unidad;
+    }
+}
